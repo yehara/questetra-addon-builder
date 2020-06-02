82ee6cd25619603210bf2dc452050f5cd4f346d9
yehara <yoshinori.ehara@gmail.com>
2020-06-02 22:39:12 +0900

    最低限動くものを実装

---
+++ README.md
@@ -0,0 +1,7 @@
+# Questetra Addon Builder
+
+以下のファイルを配置したディレクトリで ```qab``` コマンドを実行すると、Questetra に登録可能なアドオン定義ファイルが生成されます。
+
+ * addon.json - 設定ファイル
+ * src/main.js - サービスタスクのスクリプト
+ * src/icon.png - （オプション）アイコンファイル
---
+++ index.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const builder = require('xmlbuilder');
+const sharp = require('sharp');
+const dateFormat = require('dateformat');
+
+const ENGINE_TYPES = {
+    "RHINO": 0,
+    "NASHORN": 1,
+    "GRAALJS": 2
+};
+const CONFIG_OPTIONAL_ATTRIBUTES = [
+    "form-type",
+    "el-enabled",
+    "editable",
+    "select-data-type",
+    "oauth2-setting-name"
+];
+
+const basedir = process.env.PWD;
+const addon = require(basedir + '/addon.json');
+
+main();
+
+async function main() {
+
+    const xml = await buildXml();
+    const targetDir = basedir + "/build";
+    if(!fs.existsSync(targetDir)) {
+        fs.mkdirSync(targetDir);
+    }
+    const target = targetDir + "/" + addon.name + ".xml";
+    fs.writeFileSync(target, xml);
+    console.log("build completed successfully: " + target);
+
+}
+
+async function buildXml() {
+
+    const root = builder.create('service-task-definition');
+
+    root.ele('engine-type', ENGINE_TYPES[addon["engine-type"]]);
+    root.ele('last-modified', dateFormat(new Date(), 'yyyy-mm-dd'));
+
+    addMultilingualElement(addon, 'label', root);
+    if(addon['summary']) {
+        addMultilingualElement(addon, 'summary', root);
+    }
+    if(addon['help-page-url']) {
+        addMultilingualElement(addon, 'help-page-url', root);
+    }
+    if (addon.configs) {
+        const configs = root.ele('configs');
+        addon.configs.forEach(element => {
+            const config = configs.ele('config');
+            config.att('name', element.name);
+            config.att('required', element.required ? "true" : "false");
+            CONFIG_OPTIONAL_ATTRIBUTES.forEach(key => {
+                if (element.key) {
+                    config.add(key, element.key);
+                }
+            });
+            addMultilingualElement(element, 'label', config);
+        });
+    }
+
+    var script = fs.readFileSync(basedir + "/src/main.js", 'utf8');
+    root.ele('script').cdata("\n" + script + "\n");
+
+    if(addon.icon) {
+        await loadIcon(basedir + "/src/" + addon.icon, root)
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    return root.end({pretty: true});
+
+}
+
+function addMultilingualElement(obj, name, xmlRoot) {
+    xmlRoot.ele(name, obj[name]);
+    const prefix = name + "-";
+    Object.keys(obj)
+        .filter(key => key.startsWith(prefix))
+        .forEach(key => {
+            const locale = key.substring(prefix.length);
+            const element = xmlRoot.ele(name, obj[key]);
+            element.att('locale', locale);
+        });
+}
+
+async function loadIcon(path, xmlRoot) {
+    if(!fs.existsSync(path)) {
+        return Promise.reject("icon file not found: " + path);
+    }
+    return sharp(path).resize(64, 64).toBuffer().then( data => {
+        const base64 = data.toString('base64');
+        xmlRoot.ele('icon', base64);
+    }).catch(reason => {
+        console.log(reason);
+    });
+}
